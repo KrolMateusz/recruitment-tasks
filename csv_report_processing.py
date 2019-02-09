3c49663719adcdd31e1ca98a0e2d2940b7bf4db8
KrolMateusz <matek8952@gmail.com>
2019-02-09 13:39:23 +0100

    Encoding and checking files

--- csv_report_processing.py
+++ csv_report_processing.py
@@ -1,9 +1,36 @@
 import pycountry
 import csv
 import sys
+import pathlib
 
 
-def lookup(state_name):
+def check_if_file_exists(filename):
+    try:
+        pathlib.Path(filename).resolve(strict=True)
+    except FileNotFoundError as e:
+        sys.exit(e)
+    else:
+        return True
+
+
+def check_encoding(filename):
+    try:
+        with open(filename, encoding='utf-8') as csv_file:
+            csv_file.readline()
+        encoding = 'utf-8'
+    except UnicodeError:
+        pass
+    else:
+        try:
+            with open(filename, encoding='utf-16') as csv_file:
+                csv_file.readline()
+            encoding = 'utf-16'
+        except UnicodeError:
+            pass
+        return encoding
+
+
+def get_country_code(state_name):
     try:
         alpha_3 = pycountry.subdivisions.lookup(state_name).country.alpha_3
         return alpha_3
@@ -12,14 +39,22 @@ def lookup(state_name):
 
 
 def load_data():
+    file = sys.argv[1]
+    check_if_file_exists(file)
+    encoding_type = check_encoding(file)
     try:
-        with open('input.csv', newline='', encoding='utf-8') as csv_file:
+        with open(file, newline='', encoding=encoding_type) as csv_file:
             csv_reader = csv.reader(csv_file)
-            for date, state_name, impressions, ctr in csv_reader:
+            for row in csv_reader:
+                if len(row) != 4:
+                    sys.stderr.write(f'{len(row)} values instead of 4 in row: '
+                                     f'{row}\n')
+                    continue
+                date, state_name, impressions, ctr = row
                 print(date, state_name, impressions, ctr)
-                print(lookup(state_name))
+                print(get_country_code(state_name))
     except (FileNotFoundError, UnicodeError) as e:
-        sys.stdout.write('Error: ' + str(e) + '\n')
+        sys.stderr.write('Critical error: ' + str(e) + '\n')
 
 
 def main():
