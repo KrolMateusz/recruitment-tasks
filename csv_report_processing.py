6b4bfb8395422fb5b657331654b5ee9e7f0c5a00
KrolMateusz <matek8952@gmail.com>
2019-02-09 16:21:47 +0100

    Output added

--- csv_report_processing.py
+++ csv_report_processing.py
@@ -2,6 +2,7 @@ import pycountry
 import csv
 import sys
 import pathlib
+import datetime
 
 
 def check_if_file_exists(filename):
@@ -9,8 +10,6 @@ def check_if_file_exists(filename):
         pathlib.Path(filename).resolve(strict=True)
     except FileNotFoundError as e:
         sys.exit(e)
-    else:
-        return True
 
 
 def check_encoding(filename):
@@ -38,27 +37,49 @@ def get_country_code(state_name):
         return 'XXX'
 
 
-def load_data():
-    file = sys.argv[1]
-    check_if_file_exists(file)
-    encoding_type = check_encoding(file)
+def load_data_from_file(filename):
+    encoding_type = check_encoding(filename)
     try:
-        with open(file, newline='', encoding=encoding_type) as csv_file:
+        data = {}
+        with open(filename, newline='', encoding=encoding_type) as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
                 if len(row) != 4:
                     sys.stderr.write(f'{len(row)} values instead of 4 in row: '
                                      f'{row}\n')
                     continue
-                date, state_name, impressions, ctr = row
-                print(date, state_name, impressions, ctr)
-                print(get_country_code(state_name))
-    except (FileNotFoundError, UnicodeError) as e:
-        sys.stderr.write('Critical error: ' + str(e) + '\n')
+                date, state, impressions, ctr = row
+                state = get_country_code(state)
+                date = datetime.date(int(date[6:]), int(date[:2]),
+                                     int(date[3:5]))
+                data.setdefault(date, {})
+                data[date].setdefault(state, [0, 0])
+                data[date][state][0] += int(impressions)
+                data[date][state][1] += round(float(ctr[:-1])*int(impressions)
+                                              / 100)
+        return data
+    except UnicodeError as e:
+        sys.stderr.write(f'Critical error: {e}')
+
+
+def write_data_to_file(data, filename):
+    try:
+        with open(filename, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',',
+                                    lineterminator='\n')
+            for date in sorted(data.keys()):
+                for country_code in sorted(data[date]):
+                    csv_writer.writerow([date, country_code,
+                                        *data[date][country_code]])
+    except KeyError as e:
+        sys.stderr.write(e)
 
 
 def main():
-    load_data()
+    input_file, output_file = sys.argv[1:]
+    check_if_file_exists(input_file)
+    data = load_data_from_file(input_file)
+    write_data_to_file(data, output_file)
 
 
 if __name__ == '__main__':
